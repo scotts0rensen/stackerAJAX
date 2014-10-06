850050e717421bdf9e675c58ddf6c387c0d253f1
Scott Sorensen <scott.sorensen@octanner.com>
2014-10-05 18:49:03 -0600

    implement answerer

--- app.js
+++ app.js
@@ -6,15 +6,22 @@ $(document).ready( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
+	$('.inspiration-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var tags = $(this).find("input[name='tags']").val();
+		getAnswerers(tags);
+	});
 });
 
-// this function takes the question object returned by StackOverflow 
+// this function takes the question object returned by StackOverflow
 // and creates new result to be appended to DOM
 var showQuestion = function(question) {
-	
+
 	// clone our result template code
 	var result = $('.templates .question').clone();
-	
+
 	// Set the question properties in result
 	var questionElem = result.find('.question-text a');
 	questionElem.attr('href', question.link);
@@ -35,8 +42,36 @@ var showQuestion = function(question) {
 													question.owner.display_name +
 												'</a>' +
 							'</p>' +
- 							'<p>Reputation: ' + question.owner.reputation + '</p>'
+							'<p>Reputation: ' + question.owner.reputation + '</p>'
 	);
+
+	return result;
+};
+
+
+// this function takes the answerer object returned by StackOverflow
+// and creates new result to be appended to DOM
+var showAnswerer = function(answerer) {
+
+	// clone our result template code
+	var result = $('.templates .answerer').clone();
+
+	// -- name
+	var nameElem = result.find('.name-text a');
+	nameElem.attr('href', answerer.user.link);
+	nameElem.text(answerer.user.display_name);
+
+	// -- reputation
+	var reputation = result.find('.reputation');
+	reputation.text(answerer.user.reputation);
+
+	// -- post-count
+	var post_count = result.find('.post-count');
+	post_count.text(answerer.post_count);
+
+	// -- score
+	var score = result.find('.score');
+	score.text(answerer.score);
 
 	return result;
 };
@@ -59,13 +94,13 @@ var showError = function(error){
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
 var getUnanswered = function(tags) {
-	
+
 	// the parameters we need to pass in our request to StackOverflow's API
 	var request = {tagged: tags,
 								site: 'stackoverflow',
 								order: 'desc',
 								sort: 'creation'};
-	
+
 	var result = $.ajax({
 		url: "http://api.stackexchange.com/2.2/questions/unanswered",
 		data: request,
@@ -87,6 +122,37 @@ var getUnanswered = function(tags) {
 		$('.search-results').append(errorElem);
 	});
 };
+
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow
+var getAnswerers = function(tags) {
+
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = { site: 'stackoverflow' };
+
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/" + tags + "/top-answerers/all_time",
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result){
+		var searchResults = showSearchResults(tags, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item) {
+			var answerer = showAnswerer(item);
+			$('.results').append(answerer);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
+
 
 
 
